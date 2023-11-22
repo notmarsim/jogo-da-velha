1c9a315a9459bd02588651eca0711a05b5320095
marsim <141268744+notmarsim@users.noreply.github.com>
2023-11-21 23:27:50 -0300

    release

--- velha.c
+++ velha.c
@@ -5,10 +5,9 @@
 
 char tabuleiro[3][3];
 int game_over;
-int i;
-int j;
-int movement;
-int position;
+int i,j;
+int movement,position;
+
 
 void printar_tabuleiro(){
     for (i=0;i<3;i++){
@@ -19,35 +18,25 @@ void printar_tabuleiro(){
     }
 }
 
-void maquina(){
-    srand(time(NULL));
-    position = (rand()%5)+1;      // 1~5 
-    switch(position){
-        case 1:
-            printf("1\n");
-            tabuleiro[0][0] = 'x';
-            break;
-        case 2:
-            printf("2\n");
-            tabuleiro[0][2] = 'x';
-            break;
-        case 3:
-            printf("3\n");
-            tabuleiro[1][1] = 'x';
-            break;
-        case 4:
-            printf("4\n");
-            tabuleiro[2][0] = 'x';
-            break;
-        case 5:
-            printf("5\n");
-            tabuleiro[2][2] = 'x';
-            break;
-        }
+void player1(){
+    printf("player 1 to play\n");
+    int ask = 1;
+    int x,y;
+    do{
+    printf("choose a place to play\n");
+    scanf("%d %d",&x,&y);
+    if(x<0 || x>2 || y<0||y>2 ||tabuleiro[x][y]!= '.'){
+        printf("invalid play\n");
+    }
+    else{
+        tabuleiro[x][y]='x';
+        ask=0;
+    }
+    }while(ask==1);
 
 }
-void player(){
-    printf("player\n");
+void player2(){
+    printf("player 2 to play\n");
     int ask = 1;
     int x,y;
     do{
@@ -62,34 +51,71 @@ void player(){
     }
     }while(ask==1);
 }
-
+int verify(){
+    int win = 0;
+    int emptySpaces = 0;
+    for(i=0;i<3;i++){
+        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2] && tabuleiro[i][0] != '.') {
+            win++;
+        }
+        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i] && tabuleiro[0][i] != '.') {
+            win++;
+        }
+    }
+    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != '.') {
+        win++;
+    }
+    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != '.') {
+        win++;
+    }
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (tabuleiro[i][j] == '.') {
+                emptySpaces++;
+            }
+        }
+    }
+    if (win == 0 && emptySpaces == 0) {
+        printf("tie");
+        game_over = 1;
+    }
+    return win > 0 ? 1 : 0;
+    
+}
 int jogo() {
     if(movement == 0){ //maquina começa
-        maquina();
-    
+        player1();   
       }else{
-        player();
-        
+        player2();      
     }
 }
+
+
 
 int main(){  
     game_over = 0;
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             tabuleiro[i][j] = '.';
+            
         }
     }
     srand(time(NULL));
     movement = rand() % 2;       
     while(!game_over){
     
-    jogo();
-    printar_tabuleiro();
-    movement = 1 - movement;
+        jogo();
+        printar_tabuleiro();
+        movement = 1 - movement;
+        if(verify()==1){
+            if (movement==1){
+            printf("player 1 wins");
+        
+            }else{
+                printf("player 2 wins");
+            }
+        game_over = 1;
+        }
     }
-    
-    
-
     return 0;
 }
